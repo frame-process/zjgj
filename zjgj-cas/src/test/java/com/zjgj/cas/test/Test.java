a943f993d6227f03544097f4947f51668f5ecae1
liuguangsheng2 <liuguangsheng1@jd.com>
2018-08-10 13:53:13 +0800

    init-commit

---
+++ zjgj-cas/src/test/java/com/zjgj/cas/test/Test.java
@@ -0,0 +1,10 @@
+package com.zjgj.cas.test;
+
+
+public class Test {
+	
+	@org.junit.Test
+	public void test() {
+		System.out.println("junit test");
+	}
+}
---
+++ zjgj-uc/src/main/java/com/zjgj/uc/App.java
@@ -0,0 +1,13 @@
+package com.zjgj.uc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
